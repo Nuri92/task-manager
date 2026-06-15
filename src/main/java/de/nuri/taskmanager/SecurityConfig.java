a72b7ca96310a7aebbc3fe29af1e68bc287f198b
Noor Ahmed Barekzai <nuri_nb@hotmail.de>
2026-06-16 00:37:01 +0200

    Integrate JWT authentication filter

--- src/main/java/de/nuri/taskmanager/SecurityConfig.java
+++ src/main/java/de/nuri/taskmanager/SecurityConfig.java
@@ -8,9 +8,18 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+	
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
+	
+	public SecurityConfig(
+			JwtAuthenticationFilter jwtAuthenticationFilter
+	) {
+		this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+	}
 	
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -22,6 +31,9 @@ public class SecurityConfig {
 				.authorizeHttpRequests(auth -> auth
 						.requestMatchers("/auth/**").permitAll()
 						.anyRequest().authenticated()
+				).addFilterBefore(
+						jwtAuthenticationFilter,
+						UsernamePasswordAuthenticationFilter.class
 				)
 				.httpBasic(AbstractHttpConfigurer::disable)
 				.formLogin(AbstractHttpConfigurer::disable)
