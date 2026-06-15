5ce562cb7303682045c789cca70e421f1ac43e53
Noor Ahmed Barekzai <nuri_nb@hotmail.de>
2026-06-15 04:36:26 +0200

    Implement user login with password verification

--- src/main/java/de/nuri/taskmanager/SecurityConfig.java
+++ src/main/java/de/nuri/taskmanager/SecurityConfig.java
@@ -3,6 +3,8 @@ package de.nuri.taskmanager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,9 +14,18 @@ public class SecurityConfig {
 	
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		return http.csrf(csrf -> csrf.disable())
-		           .authorizeHttpRequests(auth -> auth.requestMatchers("/auth/register").permitAll()
-		                                              .anyRequest().authenticated()).build();
+		return http
+				.csrf(AbstractHttpConfigurer::disable)
+				.sessionManagement(session -> session
+						.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				)
+				.authorizeHttpRequests(auth -> auth
+						.requestMatchers("/auth/**").permitAll()
+						.anyRequest().authenticated()
+				)
+				.httpBasic(AbstractHttpConfigurer::disable)
+				.formLogin(AbstractHttpConfigurer::disable)
+				.build();
 	}
 	
 	@Bean
